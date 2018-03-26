92c78fffcb16f0a387830739ec4c29dc66c547b5
gazali <yuujinnx@gmail.com>
2018-03-26 15:10:37 +0800

    finish fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	var a, b, c = 0, 1, 1
+	return func() int {
+		ret := a
+		a, b= b, c
+		c = a+b
+		return ret
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
