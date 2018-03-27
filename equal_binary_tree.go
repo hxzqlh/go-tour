84aaa2c67e5e21f26fc83c3e147f6240f7b7c64e
gazali <yuujinnx@gmail.com>
2018-03-27 10:18:23 +0800

    finish equal binary tree

---
+++ equal_binary_tree.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
+
+func Dfs(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	Dfs(t.Left, ch)
+	ch <- t.Value
+	Dfs(t.Right, ch)
+}
+
+func Walk(t *tree.Tree, ch chan int) {
+	Dfs(t, ch)
+	close(ch)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		v1, ok1 := <- ch1
+		v2, ok2 := <- ch2
+		switch {
+		case ok1 != ok2:
+			return false
+		case ok1 == false:
+			return true
+		case v1 != v2:
+			return false
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
