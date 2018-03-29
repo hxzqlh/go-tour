ae73592b30047b30cc0fecedd19fd88b1347a014
caidongsen <1030683070@qq.com>
2018-03-29 13:05:05 +0800

    切片

---
+++ slice.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	ret := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		ret[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			ret[i][j] = uint8(i * j)
+		}
+	}
+	return ret
+}
+
+func main() {
+	pic.Show(Pic)
+}
