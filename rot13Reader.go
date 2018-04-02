c08f08f62b15fef0f464f8bcdaca374a2df521fc
wangkaifeng <2577313224@qq.com>
2018-04-02 19:34:39 +0800

    练习：rot13Reader

---
+++ rot13Reader.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rot13(b byte) byte {
+	switch {
+	case 'A' <= b && b <= 'M':
+		b = b + 13
+	case 'M' < b && b <= 'Z':
+		b = b - 13
+	case 'a' <= b && b <= 'm':
+		b = b + 13
+	case 'm' < b && b <= 'z':
+		b = b - 13
+	}
+	return b
+}
+
+func (mr rot13Reader) Read(b []byte) (int, error) {
+	n, e := mr.r.Read(b)
+	for i := 0; i < n; i++ {
+		b[i] = rot13(b[i])
+	}
+	return n, e
+}
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
