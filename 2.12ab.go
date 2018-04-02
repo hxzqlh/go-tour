c5fd9bf3747bcc1bdad20e564b91ed4a29bba78d
jjtac <y1730900103@gmail.com>
2018-04-02 18:49:42 +0800

    fix 12ab

--- 2.12ab.go
+++ 2.12ab.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	for i := 0;i<100;i+=2 {
-		fmt.Printf("%d%d%s%s",(i+1) % 10, (i+2) % 10, string(byte(i % 26)+'A'), string(byte((i+1) % 26)+'A'))
+	for i := 0;i<1000;i+=2 {
+		fmt.Printf("%d%d%s%s",i+1, i + 2, string(byte(i % 26)+'A'), string(byte((i+1) % 26)+'A'))
 	}
 }
