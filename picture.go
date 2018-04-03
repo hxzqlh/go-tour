b8af29d9dd803b2706341fdde93c56603b72f109
wangkaifeng123 <37789096+wangkaifeng123@users.noreply.github.com>
2018-04-03 18:33:16 +0800

    Update picture.go

--- picture.go
+++ picture.go
@@ -6,18 +6,15 @@ import (
 	"image/color"
 )
 
-// 1  新建构造体
 
 type Image struct{}
 
-// 2 实现官方image的三个方法
 
 func (i Image) ColorModel() color.Model {
 	return color.RGBAModel
 }
 
 func (i Image) Bounds() image.Rectangle {
-	// 这里的200（宽 高）我写死了 仅仅是展示作用  正确做法是从 i 中获取
 	return image.Rect(0, 0, 200, 200)
 }
 
