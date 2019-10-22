429974fa5d2cb34c1d85162e3d73761848a9bc2c
cj-go <cj-go@33.cn>
2019-10-22 15:30:55 +0800

    finish equal_binary_tree.go

---
+++ equal_binary_tree.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk 步进 tree t 将所有的值从 tree 发送到 channel ch。
+func Walk(t *tree.Tree, ch chan int) {
+	walk(t, ch)
+	close(ch)
+}
+
+//中序遍历 tree
+func walk(t *tree.Tree, ch chan int) {
+	if t != nil {
+		walk(t.Left, ch)
+		ch <- t.Value
+		walk(t.Right, ch)
+	}
+}
+
+// Same 检测树 t1 和 t2 是否含有相同的值。
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <-ch2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for i := range ch {
+		fmt.Println(i)
+	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
