e458baa6fc1f379130b31f7d5f50b6c4128685aa
TanCrystal <41179228+TanCrystal@users.noreply.github.com>
2019-12-01 22:59:42 +0800

    Update rot13Reader.go

---
+++ rot13Reader.go
@@ -0,0 +1,16 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: 给 MyReader 添加一个 Read([]byte) (int, error) 方法
+
+func (myReader MyReader) Read(b []byte)(int,error){
+	b[0] = 'A'
+	return 1,nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
